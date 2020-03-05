23d69ab8f2a455a5117a52e7a9a111c75f40de73
appmonster007 <prathitlenovoa1000@gmail.com>
2020-03-05 21:34:54 +0530

    runnable code for strings

--- fuckeskar.c
+++ fuckeskar.c
@@ -6,7 +6,7 @@
 
 	typedef struct noe{
 		struct noe* next;
-		char* alp;
+		char* alp ;
 	}wordlist;
 
 	typedef struct dnoe{
@@ -14,37 +14,18 @@
 		struct dnoe* nextchar;
 		char alp;
 	}dict;
-
-
-
-// 	void appendn(char* word, wordlist** ptr){
-// 		wordlist* temp = (wordlist*)malloc(sizeof(wordlist)); 
-// 		temp->alp=word;
-// 		temp->next=NULL;
-// 		wordlist* t2;
-// 		t2=*ptr;
-// 		if(*ptr==NULL){
-// 		    *ptr=temp;
-// 		}
-// 		else{
-// 		    while(t2->next!=NULL){
-// 			    t2=t2->next;
-// 		    }
-// 		    t2->next=temp;
-// 		}
-// 		return;
-// 	}
 	
 	void appendn(char* data,wordlist** head_ref) 
     { 
         wordlist *new_node = (wordlist*)malloc(sizeof(wordlist)); 
         wordlist *last = *head_ref;  
+		new_node->alp = (char*)malloc(sizeof(char)*20);
         new_node->alp  = data; 
         new_node->next = NULL; 
         if (*head_ref == NULL) 
         { 
            *head_ref = new_node; 
-           return; 
+           //return; 
         }   
         while (last->next != NULL) 
             last = last->next; 
@@ -53,31 +34,7 @@
     }  
     
 	wordlist* scan_file(char* file){
-		
-//     	FILE * fd = fopen(file, "r");
-//     	// if (fd == NULL) return 1;
-//     	char c,temp[maxlenwrd];
-//     	int count = 0;
 
-// 		wordlist *head = (wordlist*)malloc(sizeof(wordlist));
-
-
-//     	while((c = fgetc(fd)) != EOF)
-//     	{
-//     		if(c == ' ' || c == '\n' || c=='-' )
-//     		{
-//     			appendn(temp,&head);
-//                 strcpy(temp,"");
-//     		}
-//     		else if(c!='.' && c!='!' && c!='?' && c!=';' && c!='\'' && c!=',' && c!='"' && c!='(' && c!='<' && c!=')' && c!='>' && c!='{' && c!='}' && c!='[' && c!=']')
-//     		{
-//     			strncat(temp,&c,1);
-//     		}
-//     	}
-// 		appendn(temp,&head);
-//         strcpy(temp,"");
-		
-//     	fclose(fd);
 
         char fin[maxnowrd][maxlenwrd];
     	FILE * fp = fopen("sample.txt", "r");
@@ -104,54 +61,29 @@
 		}
     	fclose(fp);
         int i;
-        wordlist *head = (wordlist*)malloc(sizeof(wordlist));
-        for(i=0; i<count; i++){
+        wordlist *head =  (wordlist*)malloc(sizeof(wordlist));
+        for(i=0; i<=count; i++){
             appendn(fin[i], &head);
-        }
-        while(head->next!=NULL){
-			printf("%s\n",head->alp);
+		}	
+		
+		head = head->next;
+		wordlist *tem = head;
+        while(tem->next!=NULL){
+			printf("%s\t",tem->alp);
+			tem=tem->next;
 		}
     	return head;
 	}
      
     int main()
     {
-    	// This program reads a file from its arguments and prints a word by word. Additionally, it counts the words in the file.
-    	// char fin[maxnowrd][maxlenwrd];
-    	// FILE * fp = fopen("sample.txt", "r");
-    	// if (fp == NULL) return 1;
-    	// char c,temp[maxlenwrd];
-    	// int count = 0;
-    	// while((c = fgetc(fp)) != EOF)
-    	// {
-    	// 	if(c == ' ' || c == '\n' || c=='-' )
-    	// 	{
-    	// 		strcpy(fin[count],temp);
-        //         strcpy(temp,"");
-    	// 		++count;
-    	// 	}
-    	// 	else if(c!='.' && c!='!' && c!='?' && c!=';' && c!='\'' && c!=',' && c!='"' && c!='(' && c!='<' && c!=')' && c!='>' && c!='{' && c!='}' && c!='[' && c!=']')
-    	// 	{
-    	// 		strncat(temp,&c,1);
-    	// 	}
-    	// }
-		// if(strcmp(fin[count],temp)!=0){
-		// 	strcpy(fin[count],temp);
-        //     strcpy(temp,"");
-    	// 	++count;
-		// }
-    	// fclose(fp);
-        //start fileread check
-        // int i;
-        // for(i=0; i<count; i++){
-        //     printf("%s\n",fin[i]);
-        // }
-        //end file read check
-		wordlist *p;
-	 	p=scan_file("sample.txt");
-// 		while(p->next!=NULL){
-// 			printf("%s\n",p->alp);
-// 		}
+		wordlist *diary;
+	 	diary=scan_file("sample.txt");
+		 dict=scan_file("dict.txt");
+ 		while(p->next!=NULL){
+			 printf("%s\n",p->alp);
+			 p= p->next;
+ 		}
 
     	return 0;
     }
